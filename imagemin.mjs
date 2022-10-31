83262a3c3ad7896545d2b389435c8248f1731354
Manuel Bartels <git@manuelbartels.de>
2022-10-31 17:31:00 +0100

    fixed del quality argument

--- imagemin.mjs
+++ imagemin.mjs
@@ -2,9 +2,24 @@ import imagemin from 'imagemin';
 import mozjpeg from 'imagemin-mozjpeg';
 import pngquant from 'imagemin-pngquant';
 import svgo from 'imagemin-svgo';
-import del from 'del';
+import {deleteAsync as del} from 'del';
 import fs from 'fs';
 
+const config = {
+  quality: 85,
+};
+
+const getParam = (searchString) => {
+  const args = process.argv.slice(2);
+  const param = args.find(arg => arg.startsWith(searchString));
+  if (param) {
+    const arr = param.split('=');
+    if (arr.length === 1) return true;
+    else if (arr.length === 2) return arr[1];
+  }
+
+  return null;
+};
 
 const readFolders = (dir) => {
   return new Promise((resolve) => {
@@ -16,15 +31,17 @@ const readFolders = (dir) => {
       resolve(folders);
     });
   });
-}
+};
 
 const minimize = async (folder) => {
   const distFolder = folder.replace('./src/', '').replace('./src', '');
+  const qLow = (config.quality - 10 > 1) ? (config.quality - 10) / 100 : 0.1;
+  const qHigh = (config.quality + 10 < 100) ? (config.quality + 10) / 100 : 1;
   await imagemin([`${folder}/*.(jpg|JPG|jpeg|JPEG|png|PNG|svg|SVG)`], {
     destination: `dist/${distFolder}`,
     plugins: [
-      mozjpeg({progressive: true, quality: 85}),
-      pngquant({speed: 6, quality: [0.75, 0.90]}),
+      mozjpeg({ progressive: true, quality: config.quality }),
+      pngquant({ speed: 6, quality: [qLow, qHigh] }),
       svgo({
         plugins: [
           {removeViewBox: false},
@@ -47,6 +64,11 @@ const imageminFolder = async (src) => {
 }
 
 (async () => {
+  let q = getParam('q');
+  if (!Number.isNaN(q)) q = Number.parseInt(q);
+  else q = null;
+
+  if (q < 100 && q > 0) config.quality = q;
   console.info('delete dist');
   await del('./dist/**/*');
   minimize('./src');
